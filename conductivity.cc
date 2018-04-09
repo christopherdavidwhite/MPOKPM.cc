06e4279e02dcf79f2f5ca1146431db35104404ca
Christopher White <christopher.white@alumni.rice.edu>
2018-04-09 17:44:22 -0400

    make memory-profligate version optional

--- conductivity.cc
+++ conductivity.cc
@@ -38,6 +38,7 @@ int main(int argc, char **argv)
   uint s = 0;
   double hz = 1.0;
   double cutoff = 1e-14;
+  bool profligate = false; //use memory-profligate strategy? 
   std::string filename = "/tmp/conductivity.txt";
 
 
@@ -58,7 +59,7 @@ int main(int argc, char **argv)
       /* getopt_long stores the option index here. */
       int option_index = 0;
 
-      char c = getopt_long (argc, argv, "L:M:N:h:f:e:", long_options, &option_index);
+      char c = getopt_long (argc, argv, "L:M:N:h:f:e:p", long_options, &option_index);
 
       /* Detect the end of the options. */
       if (c == -1)
@@ -71,6 +72,10 @@ int main(int argc, char **argv)
           if (long_options[option_index].flag != 0)
             break;
 
+	case 'p':
+	  profligate = true;
+	  break;
+	  
         case 'L':
 	  L = std::stoi(optarg);
 	  std::cout << "L = " << L << "\n";
@@ -190,7 +195,12 @@ int main(int argc, char **argv)
   auto j = IQMPO(j_ampo);
 
   auto t0 = std::chrono::high_resolution_clock::now();
-  auto mu = memoryprofligate_all_double_mu(H, j, realmu_file, imagmu_file, chebbd_file, N, Maxm, cutoff, 1);
+  if(profligate){
+    auto mu = memoryprofligate_all_double_mu(H, j, realmu_file, imagmu_file, chebbd_file, N, Maxm, cutoff, 1);
+  } else {
+    auto mu = all_double_mu(H, j, realmu_file, imagmu_file, chebbd_file, N, Maxm, cutoff, 1);
+  }
+  
   auto t1 = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> computation_time = t1 - t0;
   timing_file << computation_time.count() << "\n";
