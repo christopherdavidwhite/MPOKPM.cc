6632fa997610f43ffbc029b002d47446be291fc4
Christopher White <christopher.white@alumni.rice.edu>
2018-04-27 14:12:48 -0400

    "globals" file holding (now) simulation start time t0

---
+++ globals.h
@@ -0,0 +1,11 @@
+
+#ifndef MPOKPM_GLOBALS
+#define MPOKPM_GLOBALS
+
+#include <chrono>
+
+//simulation start time
+std::chrono::time_point<std::chrono::high_resolution_clock> t0;
+
+
+#endif //MPOKPM_GLOBALS
