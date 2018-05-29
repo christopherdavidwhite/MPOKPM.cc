5634030afbbbc099878c04bbb1a5dc746d19a8a4
Christopher White <christopher.white@alumni.rice.edu>
2018-05-29 11:36:23 -0700

    Refactor to write to disk
    Initial work on a larger refactoring to two-stage process:
      (1) construct the dangler algebra and write to disk, then
      (2) analyze the dangler every which way
    This change does enables (1) but not (2) (that's next) and breaks a
    whole bunch of stuff.

--- chebyshev.cc
+++ chebyshev.cc
@@ -172,44 +172,31 @@ advance_chebyshevs(MPOt<Tensor>& Tn,
 
 template <class Tensor>
 MPOt<Tensor>
-advance_dangler_chebyshevs(MPOt<Tensor> cheb, int n0, MPOt<Tensor> H,
-			   int N, int Maxm, double cutoff,
-			   std::ofstream& chebbd_file,
-			   int prog_per)
+advance_dangler_chebyshevs(MPOt<Tensor>& cheb, MPOt<Tensor> iter,
+			   int n0, int Maxm, double cutoff)
 {
-  auto I = eye(H.sites());
-
-  MPOt<Tensor> iter = oplus(I,H);
-
-  cheb.orthogonalize();
-  iter.orthogonalize();
-
   //n is the number of Chebyshevs we've already computed.
   //note that this n is notionally a 1-index: "T1" is identity
-  for(int n = n0; n < n0 + N; n++)
-    {
-      if (n % prog_per == 0) { std::cout << n << " " << maxM(cheb) << "\n"; }
-      MPOt<Tensor> store;
+  int n = n0;
+  MPOt<Tensor> store;
 
-      auto i = findtype(cheb.A(1), Dangler);
-      auto j = findtype(iter.A(1), Dangler);
+  auto i = findtype(cheb.A(1), Dangler);
+  auto j = findtype(iter.A(1), Dangler);
 
-      IQIndex id = i.dag();
-      IQIndex jd = j.dag();
-      IQIndex k  = IQIndex("iq", Index("i",n+1, Dangler, 0), QN(0));
+  IQIndex id = i.dag();
+  IQIndex jd = j.dag();
+  IQIndex k  = IQIndex("iq", Index("i",n+1, Dangler, 0), QN(0));
 
-      //bc implementing Cheb. iterations to tensor on
-      auto iterbc = Tensor(k, id,jd);
-      //keep things multiplied by identity
-      for(int l = 1; l <= n; l++) { iterbc.set(k(l),id(l),jd(1), 1.0); }
-      iterbc.set(k(n+1), id(n),   jd(2), 2.0);  //2*H*Tn
-      iterbc.set(k(n+1), id(n-1), jd(1), -1.0); // - Tn-1
+  //bc implementing Cheb. iterations to tensor on
+  auto iterbc = Tensor(k, id,jd);
+  //keep things multiplied by identity
+  for(int l = 1; l <= n; l++) { iterbc.set(k(l),id(l),jd(1), 1.0); }
+  iterbc.set(k(n+1), id(n),   jd(2), 2.0);  //2*H*Tn
+  iterbc.set(k(n+1), id(n-1), jd(1), -1.0); // - Tn-1
       
-      nmultMPAlgebra(cheb, iter, iterbc, store, {"Maxm", Maxm, "Cutoff", cutoff}); 
-      cheb = store; //copies! Bad! Can go do rvalue magic...
+  nmultMPAlgebra(cheb, iter, iterbc, store, {"Maxm", Maxm, "Cutoff", cutoff}); 
+  cheb = store; //copies! Bad! Can go do rvalue magic...
 
-      chebbd_file << n << " " << maxM(cheb) << "\n" << std::flush;
-    }
   return cheb;
 }
 
@@ -564,6 +551,10 @@ dangler_all_double_mu(MPOt<Tensor> const& H,
   for(int i = 0; i < N_step; i++){
     
     cheb = advance_dangler_chebyshevs(cheb, N_sofar, H, N_save, Maxm, cutoff, chebbd_file, prog_per);
+    bool rl = true;
+    for(int a = 1; a < H.N(); a++) rl = rl && isReal(cheb.A(a));
+    std::cout << "real " << rl << "\n" << std::flush;
+    std::cout << "false " <<  false << "\n";
     N_sofar += N_save;
     
     IQTensor chtr = single_mu(cheb, I);
@@ -608,6 +599,52 @@ dangler_all_double_mu(MPOt<Tensor> const& H,
   }
   
   return vecmu;
+}
+
+template <class Tensor>
+MPOt<Tensor>
+listandwrite_dangler(MPOt<Tensor> const& H,
+		      std::string const& filename,
+		      int N, int Maxm, double cutoff, int prog_per)
+{
+  // macro OPENE declares second arg and initializes with filehandle to
+  // firstarg. Defined in util.cc
+  OPENE(filename + ".tim", timing_file);
+  OPENE(filename + ".chM", chebbd_file);
+
+  std::cout << "using dangler\n";
+
+  int N_sofar = 2;
+  bool writep = true;
+  
+  auto I = eye(H.sites());
+  MPOt<Tensor> cheb = oplus(I,H);
+  MPOt<Tensor> iter = oplus(I,H);
+
+  //normalization
+  int L = H.N();
+  cheb = cheb*pow(2.0, -L/2);
+  cheb.orthogonalize();
+  iter.orthogonalize();
+  
+  for(int i = 0; i < N; i++){
+    cheb = advance_dangler_chebyshevs(cheb, iter, N_sofar, Maxm, cutoff);
+    N_sofar++;
+    
+    //if we've hit the ceiling, quit: results will be junk.
+    if(Maxm >= maxM(cheb)) { writep = false; break;}
+
+    chebbd_file << N_sofar << " " << maxM(cheb) << "\n" << std::flush;
+    
+    //performance information
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> computation_time = t1 - t0;
+    timing_file << N_sofar << " " << computation_time.count() << "\n" << std::flush;
+    if ((0 == N_sofar % prog_per) && writep) { writeToFile(filename + ".chMPA", cheb); }
+  }
+  
+  if (writep) { writeToFile(filename + ".chMPA", cheb); }
+  return cheb;
 }
 
 #endif //#ifndef MPOKPM_CHEBYSHEV
